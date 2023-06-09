8a7b11c7bb78fe583971ca3bfbd202a3e30ebadc
Spoolae <ohotnic147feed@gmail.com>
2023-06-09 10:15:27 +0200

    Refactoring registration page

--- client/src/components/Header/AuthenticationHeader.jsx
+++ client/src/components/Header/AuthenticationHeader.jsx
@@ -6,29 +6,20 @@ import CONSTANTS from '../../constants';
 import Logo from '../Logo/Logo';
 
 const AuthenticationHeader = ({ authMode }) => {
-  if ((authMode = CONSTANTS.AUTH_MODE.LOGIN)) {
-    return (
-      <div className={styles.headerSignUpPage}>
-        <Logo src={`${CONSTANTS.STATIC_IMAGES_PATH}logo.png`} alt="logo" />
-        <div className={styles.linkLoginContainer}>
-          <Link to="/registration" style={{ textDecoration: 'none' }}>
-            <span>Signup</span>
-          </Link>
-        </div>
+  const linkPath =
+    authMode === CONSTANTS.AUTH_MODE.LOGIN ? '/registration' : '/login';
+  const linkText = authMode === CONSTANTS.AUTH_MODE.LOGIN ? 'Signup' : 'Login';
+
+  return (
+    <div className={styles.headerSignUpPage}>
+      <Logo src={`${CONSTANTS.STATIC_IMAGES_PATH}logo.png}`} alt="logo" />
+      <div className={styles.linkLoginContainer}>
+        <Link to={linkPath}>
+          <span>{linkText}</span>
+        </Link>
       </div>
-    );
-  } else {
-    return (
-      <div className={styles.headerSignUpPage}>
-        <Logo src={`${CONSTANTS.STATIC_IMAGES_PATH}logo.png`} alt="logo" />
-        <div className={styles.linkLoginContainer}>
-          <Link to="/login" style={{ textDecoration: 'none' }}>
-            <span>Login</span>
-          </Link>
-        </div>
-      </div>
-    );
-  }
+    </div>
+  );
 };
 
 export default AuthenticationHeader;
